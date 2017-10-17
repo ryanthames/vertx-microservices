a59d0a00408edb5537aae212f0ffa6985f6f6899
Ryan Thames <ryanthames@gmail.com>
2017-10-16 21:27:39 -0500

    Initial commit

---
+++ hello-microservice-http/src/main/java/io/vertx/book/http/HelloMicroservice.java
@@ -0,0 +1,35 @@
+package io.vertx.book.http;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+
+public class HelloMicroservice extends AbstractVerticle {
+
+  @Override
+  public void start() {
+    Router router = Router.router(vertx);
+    router.get("/").handler(this::hello);
+    router.get("/:name").handler(this::hello);
+
+    vertx.createHttpServer()
+        .requestHandler(router::accept)
+        .listen(8080);
+  }
+
+  private void hello(RoutingContext rc) {
+    String message = "hello";
+
+    if(rc.pathParam("name") != null) {
+      message += " " + rc.pathParam("name");
+    }
+
+    JsonObject json = new JsonObject().put("message", message);
+
+    rc.response()
+        .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+        .end(json.encode());
+  }
+}
